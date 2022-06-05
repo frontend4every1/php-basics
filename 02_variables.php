06c176ad56de3405e903cf7a3100b65c72cf4e48
azamat <frontend4every1@gmail.com>
2022-06-05 05:26:17 +0500

    variables

---
+++ 02_variables.php
@@ -0,0 +1,69 @@
+<?php
+/* ----- Variables & Data Types ----- */
+
+/* --------- PHP Data Types --------- */
+/*
+- String    - A string is a series of characters surrounded by quotes
+- Integer   - Whole numbers
+- Float     - Decimal numbers
+- Boolean   - true or false
+- Array     - An array is a special variable, which can hold more than one value
+- Object    - A class
+- NULL      - Empty variable
+- Resource  - A special variable that holds a resource
+*/
+
+/* --------- Variable Rules --------- */
+/*
+- Variables must be prefixed with $
+- Variables must start with a letter or the underscore character
+- variables can't start with a number
+- Variables can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+- Variables are case-sensitive ($name and $NAME are two different variables)
+*/
+
+// String
+$name = 'Azamat';
+var_dump($name);
+echo '<br>';
+
+// Int
+$age = 26;
+var_dump($age);
+echo '<br>';
+
+// Boolean
+$has_kids = true; // shows 1
+$has_kids = false; // doesn't show anything
+var_dump($has_kids);
+echo '<br>';
+
+// Floats
+$cash_on_hand = 19.09;
+var_dump($cash_on_hand);
+echo '<br>';
+
+echo '$name is $age years old';
+echo '<br>';
+echo $name . ' is ' . $age . ' years old';
+echo '<br>';
+echo "${name} is $age years old";
+echo '<br>';
+
+$x = '5' + '5';
+$x = '5' + 5;
+var_dump($x);
+echo '<br>';
+echo 19 - 9;
+echo '<br>';
+echo 5 * 10;
+echo '<br>';
+echo 10 / 2;
+echo '<br>';
+echo 10 % 3;
+echo '<br>';
+
+define('HOST', 'localhost');
+define('DB_NAME', 'dev_db');
+
+echo HOST;
