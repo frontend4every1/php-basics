8a63672e4d62a17103f9f01bfc9c6ab5dc1af18f
azamat <frontend4every1@gmail.com>
2022-06-06 03:05:12 +0500

    array functions

--- 07_array_functions.php
+++ 07_array_functions.php
@@ -5,3 +5,80 @@
   Functions to work with arrays
   https://www.php.net/manual/en/ref.array.php
 */
+
+$fruits = ['apple', 'banana', 'orange'];
+
+// Get array length
+//echo count($fruits);
+
+echo '<br>';
+
+// Search array
+//var_dump(in_array('banana', $fruits));
+
+// Add to an array
+$fruits[] = 'grape';
+array_push($fruits, 'mango', 'raspberry'); // Add to the end
+array_unshift($fruits, 'kiwi'); // Adds to the beginning
+
+
+// Remove from array
+array_pop($fruits); // Removes last
+array_shift($fruits); // Removes first
+
+// Remove specific element
+unset($fruits[2]);
+
+//print_r($fruits);
+
+// Split into chunks of 2
+$chunkedArray = array_chunk($fruits, 2);
+//print_r($chunkedArray);
+
+
+// Concatenate arrays
+$arr1 = [1, 2, 3];
+$arr2 = [4, 5, 6];
+$arr3 = array_merge($arr1, $arr2);
+//print_r($arr3);
+$arr4 = [...$arr1, ...$arr2]; // Use Spread
+//print_r($arr4);
+
+
+// Combine arrays (Keys & values)
+$a = ['green', 'red', 'yellow'];
+$b = ['avocado', 'apple', 'banana'];
+$c = array_combine($a, $b);
+//print_r($c);
+
+// Array of keys
+$keys = array_keys($c);
+//print_r($keys);
+
+
+// Flip keys with values
+$flipped = array_flip($c);
+//print_r($flipped);
+
+
+// Create array of numbers with range()
+$numbers = range(1, 20);
+//print_r($numbers);
+
+
+// Map through array and create a new one
+$newNumbers = array_map(function ($number) {
+    return "Number ${number}";
+}, $numbers);
+//print_r($newNumbers);
+
+
+// Filter array
+$lessThan10 = array_filter($numbers, fn($number) => $number < 10);
+print_r($lessThan10);
+
+
+// Array Reduce
+// "carry" holds the return value of the previous iteration
+$sum = array_reduce($numbers, fn($carry, $number) => $carry + $number);
+var_dump($sum);
